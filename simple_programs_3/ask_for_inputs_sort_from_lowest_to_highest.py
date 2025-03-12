86907b4696dc45d818eeb6da5e430cdf7139d38c
Mark Comia <marklestercomia22@gmail.com>
2025-03-12 15:16:03 +0800

    started P5,made try except code block that ask for input then break if invalid

---
+++ simple_programs_3/ask_for_inputs_sort_from_lowest_to_highest.py
@@ -0,0 +1,9 @@
+numbers = []
+while True:
+    value = input('')
+    try:
+        num = int(value)
+        numbers.append(num)
+    except ValueError:
+        print('Invalid input...Exiting')
+        break
