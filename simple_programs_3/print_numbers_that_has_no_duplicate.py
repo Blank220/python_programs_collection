e055e871646d79fe7926f3e0428c2e6237fe7635
Mark Comia <marklestercomia22@gmail.com>
2025-03-12 14:05:12 +0800

    create a new list that holds unique numbers,checks if numbers have duplicate by using count

--- simple_programs_3/print_numbers_that_has_no_duplicate.py
+++ simple_programs_3/print_numbers_that_has_no_duplicate.py
@@ -2,3 +2,9 @@ numbers = []
 for num in range(10):
     value = int(input())
     numbers.append(value)
+
+unique_numbers = []
+for num in numbers:
+    if numbers.count(num) == 1:
+        unique_numbers.append(num)
+print(unique_numbers)
