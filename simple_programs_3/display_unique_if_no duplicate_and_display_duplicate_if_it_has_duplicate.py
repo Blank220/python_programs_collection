72a32b27968f54d8dd5701273fae2ee76a0f6aa2
Mark Comia <marklestercomia22@gmail.com>
2025-03-12 14:43:42 +0800

    added try;except command to break the while loop. if true, it checks if the number is already in the set and prints unique or duplicate

--- simple_programs_3/display_unique_if_no duplicate_and_display_duplicate_if_it_has_duplicate.py
+++ simple_programs_3/display_unique_if_no duplicate_and_display_duplicate_if_it_has_duplicate.py
@@ -2,3 +2,14 @@ displayed_numbers = set()
 
 while True:
     value = input('')
+    
+    try:
+        number = int(value)
+        if number in displayed_numbers:
+            print('Duplicate')
+        else:
+            print('Unique')
+            displayed_numbers.add(number)
+    except ValueError:
+        print('Invalid input.....Exit')
+        break
